e4123a0fcf3a487a26ec260196b6d8166fc46e7a
pkgzx <olvadis2004@gmail.com>
2025-09-01 14:41:31 -0500

    Feat: add user validation during user creation process

--- infrastructure/entry-points/reactive-web/src/main/java/com/creadiya/authentication/api/handlers/UserHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/creadiya/authentication/api/handlers/UserHandler.java
@@ -2,6 +2,7 @@ package com.creadiya.authentication.api.handlers;
 
 import com.creadiya.authentication.api.dto.CreateUserDto;
 import com.creadiya.authentication.api.mapper.IUserMapper;
+import com.creadiya.authentication.api.validation.UserValidator;
 import com.creadiya.authentication.usecase.permission.cases.UserUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,10 +19,22 @@ import reactor.core.publisher.Mono;
 public class UserHandler {
   private final UserUseCase userCase;
   private final IUserMapper userMapper;
+  private final UserValidator validator;
 
   public Mono<ServerResponse> createUser(ServerRequest request) {
     return request.bodyToMono(CreateUserDto.class)
       .map(userMapper::toModel)
+      .flatMap(user ->
+        validator.validName(user.getName())
+          .then(validator.validLastName(user.getLastName()))
+          .then(validator.validEmail(user.getEmail()))
+          .then(validator.validAddress(user.getAddress()))
+          .then(validator.validIdentification(user.getIdentification()))
+          .then(validator.validateCurrency(user.getBaseSalary().getCurrency().getCurrencyCode()))
+          .then(validator.validPhone(user.getPhone()))
+          .then(validator.validOtherEntities(user))
+          .then(Mono.just(user))
+      )
       .flatMap(userCase::createUser)
       .flatMap(response -> ServerResponse.status(201)
         .contentType(MediaType.APPLICATION_JSON)
